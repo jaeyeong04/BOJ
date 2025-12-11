b99db74a28b32b3a624d4bcbfa9f11883e595258
jaeyeong04 <162528131+jaeyeong04@users.noreply.github.com>
2025-12-11 18:03:53 +0900

    [Bronze V] Title: A×B, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        bw.write(String.valueOf(A*B));
+        bw.flush();
+    }
+}
