2dd694f7e5c7949015f9916496a3ba475fe6349d
jaeyeong04 <162528131+jaeyeong04@users.noreply.github.com>
2025-12-19 12:33:56 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+        bw.write(String.valueOf(A+B+C));
+        bw.flush();
+    }
+}
